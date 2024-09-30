0cb180d838c4aa3f6fc3761c03820d8ae31b8d76
kafeelkazeem <kafeelkazeem@gmail.com>
2024-09-30 20:05:10 +0100

    added express validation to technician route

--- controllers/technician.js
+++ controllers/technician.js
@@ -1,11 +1,12 @@
 import Technician from "../models/technician.js"
 import { haversineDistance } from "../util/helpers.js"
 
+//middleware to get list of technician in the category the customer selected
 export const getSelectedCategory = async (req, res) =>{
     const {category, latitude, longitude} = req.query
     try {
         const technicians = await Technician.find({category: category.toLowerCase()}).select('businessName category address ratings latitude longitude')
-        const nearestTechnician = technicians.filter(i => haversineDistance(latitude, longitude, i.latitude, i.longitude) <= 10000)
+        const nearestTechnician = technicians.filter(i => haversineDistance(latitude, longitude, i.latitude, i.longitude) <= 10000) //technicians within 10km from user's position
         return res.status(200).json({nearestTechnician: nearestTechnician})
     } catch (error) {
         console.log(error)
--- routes/technician.js
+++ routes/technician.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import { getSelectedCategory } from '../controllers/technician.js'
+import { query } from 'express-validator'
+import { checkValidation } from '../middlewares/checkValidation.js'
 
 const router = express.Router()
 
-router.get('/getSelectedCategory', getSelectedCategory)
+const selectedCategoryVal = [
+    query('category').trim().toLowerCase().notEmpty(),
+    query('latitude').trim().isNumeric().notEmpty(),
+    query('longitude').trim().isNumeric().notEmpty()
+]
+
+router.get('/getSelectedCategory', selectedCategoryVal, checkValidation, getSelectedCategory)
 
 export default router
