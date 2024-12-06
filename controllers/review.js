f7e42eb3ab75ddcab9c35caf873711c058144a29
kafeelkazeem <kafeelkazeem@gmail.com>
2024-12-06 15:10:15 +0100

    completed deleteReview endpoint

--- controllers/review.js
+++ controllers/review.js
@@ -36,7 +36,28 @@ export const getTechnicianReviews = async (req, res) =>{
 export const deleteReview = async (req, res) =>{
     const {customerId, reviewId} = req.query
     try {
-        
+        const review = await Review.findById(reviewId)
+
+        if(!review){
+            return res.status(404).json({error: 'review not found'})
+        }
+
+        if(customerId !== review.customer.toString()){
+            return res.status(403).json({error: 'not authorised'})
+        }
+
+        await Review.findByIdAndDelete(reviewId)
+
+        //calculate technician's average ratings
+        const technicianId = review.technician
+        const reviews = await Review.find({technician: technicianId})
+        const totalReviews = reviews.length;
+        const avgRatings = reviews.reduce((sum, rev) => sum + rev.rating, 0) / totalReviews;
+
+        await Technician.findByIdAndUpdate(technicianId, { avgRatings, reviewCount: totalReviews});
+
+        res.status(200).json({success: 'deleted'})
+
     } catch (error) {
         console.log(error)
         res.status(500).json({error: 'internal server error'})
